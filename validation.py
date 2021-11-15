fdfea2d7a378f9827ccfb4a9289b37559b57d083
johnnyabouhaidar <johnnyabuhaydar@gmail.com>
2021-11-15 10:48:14 +0200

    finished value retreival from BM file

--- validation.py
+++ validation.py
@@ -3,10 +3,11 @@ import pandas as pd
 from openpyxl import load_workbook
 
 
-referenceExcelFile = "valid\\newBM.xlsx"
-inputFolder = "validation_folder_input"
 
+inputFolder = "validation_folder_input"
 sheetname="Sheet1"
+
+referenceExcelFile = "valid\\newBM.xlsx"
 bm_sheetname="Sheet1"
 
 cells_to_check=["E9","F9","G9","H9",
@@ -19,28 +20,35 @@ final_cells_to_check =  ["E11","F11","G11","H11",
                          "E15","F15","G15","H15",
                          "E33","F33","G33","H33",]      
 
-#for fileObj in os.listdir(inputFolder):
-#    print(fileObj)
-#    print(referenceExcelFile)
-#    wb1 = load_workbook(os.path.join(inputFolder,fileObj))
-#    ws1 = wb1[sheetname]
-#    for cell in cells_to_check:
-#        print(ws1[cell].value)
+
 
 def load_bm_file():
     df = pd.read_excel(referenceExcelFile)
     return df
 
-def check_file_if_valid(filename,reference_excel_file):
+
+def get_cell_value(initial_cell_val,data_frame):
+    returned_value = "empty"
+    for index, vall in data_frame.iterrows():
+        if initial_cell_val == vall['item']:
+            returned_value=vall['ValueNumeric']
+            break
+
+    return returned_value    
+
+def check_file_if_valid(filename):
     wb_filetoVerify = load_workbook(filename)
     ws_toverify = wb_filetoVerify[sheetname]
-    for cell in cells_to_check:
-        print(ws_toverify[cell].value)
-
     bmDF = load_bm_file()
+    
+    for cell in cells_to_check:
+        value_toWrite = get_cell_value(ws_toverify[cell].value,bmDF)
+        if value_toWrite !="empty":
+            ws_toverify[cell] = value_toWrite
+            wb_filetoVerify.save(filename)
 
 
 if __name__ == '__main__':
-    check_file_if_valid("validation_folder_input\M1_IBS_ItemNames.xlsx",referenceExcelFile)
+    check_file_if_valid("validation_folder_input\M1_IBS_ItemNames.xlsx")
 
 
